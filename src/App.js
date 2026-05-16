f114cde2b34eeec596fd2867da83739813635da2
Kamil Nowak <kamnowax@gmail.com>
2026-05-16 11:33:59 +0200

    button implemantation

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     let message;
     if (email.length < 10) {
         message = <div>Twoj adres jest za krótki!</div>;
-    } else if (email.length < 15) {
+    } else if (email.length < 25) {
         message = <div>Twój adres e-mail jest odpowiedniej dlugości.</div>;
     } else {
         message = <div>Twój adres e-mail jest stanowczo za długi.</div>;
@@ -19,12 +19,16 @@ function App() {
 
     return (
         <div>
-            <h1>System do zapisów na zajęcia</h1>
-            <h2>Twój e-mail to {email}.</h2>
-            {message}
+            <h1>Witaj w systemie do zapisów na zajęcia</h1>
+            <h2>Zaloguj sie e-mailem {email}.</h2>
+            <h3>Twój e-mail dużymi literami to {email.toUpperCase()}</h3>
             <input type="text" value={email} onChange={handleChange}/>
+            {(email.length < 10 || email.length >= 25) && <div>{message}</div>}            <button type="button" onClick={() => alert(email)}>
+                Wyświetl mój e-mail w alercie
+            </button>
 
         </div>
+
     );
 }
 export default App;
