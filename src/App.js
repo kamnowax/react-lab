2907ccfb68f341cbfff8f14a5be862abb0fc8678
Kamil Nowak <kamnowax@gmail.com>
2026-05-16 12:24:18 +0200

    Loggin in button completion

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from "react";
+import { useState } from "react";
 
 function App() {
+
     const [email, setEmail] = useState('kamnowax@gmail.com');
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     function handleChange(event) {
         setEmail(event.target.value);
     }
+
     let message;
+
     if (email.length < 10) {
         message = <div>Twoj adres jest za krótki!</div>;
     } else if (email.length < 25) {
@@ -19,23 +23,50 @@ function App() {
 
     return (
         <div>
-            <h1>Witaj w systemie do zapisów na zajęcia</h1>
-            <h2>
-                Zaloguj się e-mailem
-                <input
-                    type="text"
-                    value={email}
-                    onChange={handleChange}
-                />
-                <button type="button" onClick={() => alert(email)}>
-                    Wchodzę
-                </button>
-            </h2>
-            {(email.length < 10 || email.length >= 25) && <div>{message}</div>}
 
+            <h1>Witaj w systemie do zapisów na zajęcia</h1>
+
+            {!isLoggedIn && (
+                <div>
+
+                    <h2>Zaloguj się e-mailem</h2>
+
+                    <input
+                        type="text"
+                        value={email}
+                        onChange={handleChange}
+                    />
+
+                    <button
+                        type="button"
+                        onClick={() => setIsLoggedIn(true)}
+                    >
+                        Wchodzę
+                    </button>
+
+                    {(email.length < 10 || email.length >= 25) &&
+                        <div>{message}</div>
+                    }
+
+                </div>
+            )}
+
+            {isLoggedIn && (
+                <div>
+
+                    <h1>Witaj {email}</h1>
+
+                    <a href="#"
+                       onClick={() => setIsLoggedIn(false)}
+                    >
+                        Wyloguj
+                    </a>
+
+                </div>
+            )}
 
         </div>
-
     );
 }
+
 export default App;
